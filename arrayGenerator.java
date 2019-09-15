4a20b41fe5fc89a2465eb1abebe42be835746215
BobSaidHi <47067448+BobSaidHi@users.noreply.github.com>
2019-09-14 17:42:45 -0700

    Create arrayGenerator.java

---
+++ arrayGenerator.java
@@ -0,0 +1,20 @@
+public class arrayGenerator
+{
+    public void main(String[] args)
+    {
+        //Config
+        int size = 10;
+        //Generate It
+        a = arrayGen(size);
+    }
+    //Creates an array of the specifed size
+    public int[] arrayGen(int size)
+    {
+        int[] a = new int[size];
+            for(int i = 0; i < 3; i++)
+            {
+                a[i] = readInt("Input a number: ");
+            }
+        return a;
+    }
+}
